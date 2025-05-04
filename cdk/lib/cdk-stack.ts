467862443fe89113fc8d315cb9b6b4cbb0d7fa2f
Taichi Ishikawa <ishikawataichi1@gmail.com>
2025-05-04 14:58:47 +0900

    cdk: create lambda, IamRole

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,60 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class CdkStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
+
+		const destroyFn = new lambdaNode.NodejsFunction(
+			this,
+			createName("DestroyExecFn"),
+			{
+				runtime: lambda.Runtime.NODEJS_22_X,
+				entry: "lambda/delete-stack.ts",
+				handler: "handler",
+				timeout: cdk.Duration.minutes(5),
+				bundling: { format: lambdaNode.OutputFormat.ESM },
+			}
+		);
+
+		destroyFn.addToRolePolicy(
+			new iam.PolicyStatement({
+				actions: ["cloudformation:DeleteStack"],
+				resources: ["*"],
+			})
+		);
+
+		const schedulerInvokeRole = new iam.Role(
+			this,
+			createName("SchedulerInvokeRole"),
+			{
+				assumedBy: new iam.ServicePrincipal("scheduler.amazonaws.com"),
+				description:
+					"Allows EventBridge Scheduler to invoke DestroyExecFn",
+			}
+		);
+
+		schedulerInvokeRole.addToPolicy(
+			new iam.PolicyStatement({
+				actions: ["lambda:InvokeFunction"],
+				resources: [destroyFn.functionArn],
+			})
+		);
+
+		new cdk.CfnOutput(this, createName("DestroyExecFnArn"), {
+			value: destroyFn.functionArn,
+		});
+
+		new cdk.CfnOutput(this, createName("SchedulerInvokeRoleArn"), {
+			value: schedulerInvokeRole.roleArn,
+		});
 	}
+}
+
+function createName(name: string) {
+	const projectName = "iac-ttl";
+	return `${projectName}-${name}`;
 }
